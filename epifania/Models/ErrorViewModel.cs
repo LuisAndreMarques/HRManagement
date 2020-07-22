012f1b46c1ae406d324f7ef8d2b3bf53787315f6
António Arantes <antonio.varantes@gmail.com>
2020-07-22 11:44:38 +0100

    inicio projeto

---
+++ epifania/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace epifania.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
