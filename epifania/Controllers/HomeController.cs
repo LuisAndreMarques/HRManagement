1e8387ce63f1ee6e4e4c0e12b8907d71b2610429
Luis André Marques <luis_vsc_17@hotmail.com>
2020-09-12 17:59:50 +0100

    * Index.cshtml: * _Layout.cshtml: * HomeController.cs: * Main.cshtml:
    * Index.cshtml: Novas alterações:
    Layout
    Backoffice menu

--- epifania/Controllers/HomeController.cs
+++ epifania/Controllers/HomeController.cs
@@ -35,45 +35,4 @@ namespace Epifania.Controllers
         }
     }
 
-    public class BackOfficeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-
-        public BackOfficeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Main()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
-
-    public class FrontOfficeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-
-        public FrontOfficeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Main()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
 }
