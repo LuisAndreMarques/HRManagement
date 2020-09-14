a14c674ef46b80e5bad4fc8a38d5e87d297ad328
Luis André Marques <luis_vsc_17@hotmail.com>
2020-09-14 21:15:53 +0100

    Novo layout

---
+++ Epifania/Controllers/BackOfficeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Epifania.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Epifania.Controllers
+{
+    public class BackOfficeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public BackOfficeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult BackOfficeMain()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            
+        }
+    }
+}
+
+
